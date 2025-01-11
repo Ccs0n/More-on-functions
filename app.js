3402bebf53f92add7d1d0cb6427ba358959501c4
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-01-12 00:38:55 +0200

    first commit.

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+"use strict";
+
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('Game is starting...');
+}
+
+startGameBtn.addEventListener('click', startGame);
